afd6748bae0bc9b97d988ec9730d656ed064abf3
sbbbsnbe-arch <sbbbsnbe@gmail.com>
2026-05-28 23:11:02 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RufloApp from '../ruflo-app'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RufloApp />
+  </React.StrictMode>
+)
